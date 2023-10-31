18859c05da23999c964b1df8f890a2f24f2315d9
jrvalerio <amarovsjr81@gmail.com>
2023-10-30 23:43:17 -0300

    🧩 Tarefa 2 | Mínimo e Máximo

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function isPalindrome(str) {
-    
   str = str.replace(/\s/g, "").toLowerCase();
 
   const reversed = str.split("").reverse().join("");
@@ -12,5 +11,25 @@ console.log(isPalindrome("A sacada da casa"));
 console.log(isPalindrome("Teste de palíndromo"));
 
 function arrayMaxMin(arr) {
-  /* Seu código aqui */
+  if (arr.length === 0) {
+    return undefined;
+  }
+
+  let min = arr[0];
+  let max = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+
+  return [min, max];
 }
+
+console.log(arrayMaxMin([1, 5, 3, 9, 2]));
+console.log(arrayMaxMin([]));
+console.log(arrayMaxMin([42]));
